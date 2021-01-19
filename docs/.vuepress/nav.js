953b451eef65c5cc57b80f8bdfc1d5c341475c31
李庆松 <lqs@ecrrc.com>
2021-01-19 15:11:23 +0800

    生态link

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -30,8 +30,11 @@ module.exports = [
     {
         text: '生态',
 		items: [
-            {text: 'admin-antd-react', link: 'http://admin-antd-react.liqingsong.cc'},
             {text: 'admin-antd-vue', link: 'http://admin-antd-vue.liqingsong.cc'},
+            {text: 'admin-antd-react', link: 'http://admin-antd-react.liqingsong.cc'},
+            {text: 'electron-admin-element-vue', link: 'http://admin-element-vue.liqingsong.cc/tsv2/guide/senior/electron.html'},
+            {text: 'electron-admin-antd-vue', link: 'http://admin-antd-vue.liqingsong.cc/guide/senior/electron.html'},
+            {text: 'electron-admin-antd-react', link: 'http://admin-antd-react.liqingsong.cc/guide/senior/electron.html'},
         ]
     },
     {
