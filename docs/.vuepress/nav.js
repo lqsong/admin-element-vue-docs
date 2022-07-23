2217f8bae8d48b128cf15df405e640f07c9a562b
lqs <957698457@qq.com>
2022-07-23 12:49:17 +0800

    add 前台前端SSR解决方案列表

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -39,14 +39,30 @@ module.exports = [
     },
     {
         text: '生态',
-		items: [
-            {text: 'admin-antd-vue', link: 'http://admin-antd-vue.liqingsong.cc'},
-            {text: 'admin-antd-react', link: 'http://admin-antd-react.liqingsong.cc'},
-            {text: 'electron-admin-element-vue', link: 'http://admin-element-vue.liqingsong.cc/tsv2/guide/senior/electron.html'},
-            {text: 'electron-admin-antd-vue', link: 'http://admin-antd-vue.liqingsong.cc/webpackts/guide/senior/electron.html'},
-            {text: 'electron-admin-antd-react', link: 'http://admin-antd-react.liqingsong.cc/guide/senior/electron.html'},
-            {text: 'admin-vue3-micro-qiankun', link: 'http://admin-vue3-micro-qiankun.liqingsong.cc'},
-        ]
+        items: [
+          {
+            text: '后台框架',
+            items: [
+              {text: 'admin-element-vue', link: 'http://admin-element-vue.liqingsong.cc'},
+              {text: 'admin-antd-vue', link: 'http://admin-antd-vue.liqingsong.cc'},
+              {text: 'admin-antd-react', link: 'http://admin-antd-react.liqingsong.cc'},
+              {text: 'electron-admin-element-vue', link: 'http://admin-element-vue.liqingsong.cc/tsv2/guide/senior/electron.html'},
+              {text: 'electron-admin-antd-vue', link: 'http://admin-antd-vue.liqingsong.cc/webpackts/guide/senior/electron.html'},
+              {text: 'electron-admin-antd-react', link: 'http://admin-antd-react.liqingsong.cc/guide/senior/electron.html'},
+              {text: 'admin-vue3-micro-qiankun', link: 'http://admin-vue3-micro-qiankun.liqingsong.cc'},
+            ],
+          },
+          {
+            text: '前台框架',
+            items: [
+              {
+                text: 'midway-vue3-ssr',
+                link: 'http://midway-vue3-ssr.liqingsong.cc'
+              }
+            ],
+          },
+          
+        ],
     },
     {
         text: 'vite.ts',
